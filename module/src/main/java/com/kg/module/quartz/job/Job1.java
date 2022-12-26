6527d7bda1bc579e4b756f44d924b6afd682b284
zhaoxin <877185959@qq.com>
2022-12-26 17:32:37 +0800

    Add: 定时任务演示demo（必须实现org.quartz.Job）

---
+++ module/src/main/java/com/kg/module/quartz/job/Job1.java
@@ -0,0 +1,17 @@
+package com.kg.module.quartz.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @author ziro
+ * @date 2022-12-26 15:22:11
+ */
+public class Job1 implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("执行job1，只是demo，什么都没有============");
+    }
+}
