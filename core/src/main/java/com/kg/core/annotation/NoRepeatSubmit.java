e8afa54ee12e7bab7ab58aaa92689868b6290fea
zhaoxin <877185959@qq.com>
2023-01-07 15:04:02 +0800

    Add: 重要！新增防止表单重复提交的AOP，通过注解方式使用@NoRepeatSubmit

---
+++ core/src/main/java/com/kg/core/annotation/NoRepeatSubmit.java
@@ -0,0 +1,19 @@
+package com.kg.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 防止重复提交
+ *
+ * @author ziro
+ * @date 2023-01-07 11:05:29
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface NoRepeatSubmit {
+    /**
+     * 放重复锁时间 默认2s不能重复提交
+     */
+    long lockSecond() default 2L;
+}
