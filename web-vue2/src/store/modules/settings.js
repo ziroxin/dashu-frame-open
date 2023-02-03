1c67905728aa19ea2dd1aa847ab4327579bc5264
zhaoxin <877185959@qq.com>
2023-02-03 15:22:06 +0800

    Fix: 修复cookie中存储setting配置bug

--- web-vue2/src/store/modules/settings.js
+++ web-vue2/src/store/modules/settings.js
@@ -2,7 +2,7 @@ import variables from '@/styles/element-variables.scss'
 import defaultSettings from '@/settings'
 import Cookies from "js-cookie";
 
-const {title, showSettings, tagsView, fixedHeader, sidebarLogo} = JSON.parse(Cookies.get('settings')) || defaultSettings
+const {title, showSettings, tagsView, fixedHeader, sidebarLogo} = Cookies.get('settings') ? JSON.parse(Cookies.get('settings')) : defaultSettings
 
 const state = {
   theme: variables.theme,
