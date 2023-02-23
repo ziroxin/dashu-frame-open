3c15798dcec16961af633932d6ef78e879fbbc89
zhaoxin <877185959@qq.com>
2023-02-23 10:36:38 +0800

    Update: 优化前端白名单判断逻辑（在白名单内的页面，不需判断token）

--- web-vue2/src/permission.js
+++ web-vue2/src/permission.js
@@ -21,61 +21,62 @@ router.beforeEach(async (to, from, next) => {
   // 设置标题
   document.title = getPageTitle(to.meta.title)
 
-  // 判断用户是否登录（有token代表登录）
-  const hasToken = getToken()
+  // 路由白名单，直接跳转（不需检测token）
+  if (whiteList.indexOf(to.path) !== -1) {
+    next()
+    NProgress.done()
+  } else {
+    // 判断用户是否登录（有token代表登录）
+    const hasToken = getToken()
 
-  if (hasToken) {
-    // 判断token的有效期
-    let tokenValidTime = getTokenValidTime();
-    if ((new Date().getTime() + (10 * 60 * 1000)) > new Date(tokenValidTime).getTime()) {
-      // token失效前10分钟，刷新token
-      store.dispatch('user/refreshToken')
-    }
-    if (to.path === '/login') {
-      // 登录后，跳转到首页
-      next({path: '/'})
-      NProgress.done()
-    } else {
-      // 登录后，跳转到原来打开的页面
-      // 跳转前，先判断：store里是否有角色信息
-      const hasRouters = store.getters.perrouters && store.getters.perrouters.length > 0
-      if (hasRouters) {
-        next()
+    if (hasToken) {
+      // 判断token的有效期
+      let tokenValidTime = getTokenValidTime();
+      if ((new Date().getTime() + (10 * 60 * 1000)) > new Date(tokenValidTime).getTime()) {
+        // token失效前10分钟，刷新token
+        store.dispatch('user/refreshToken')
+      }
+      if (to.path === '/login') {
+        // 登录后，跳转到首页
+        next({path: '/'})
+        NProgress.done()
       } else {
-        try {
-          // 查询角色信息
-          const {perrouters} = await store.dispatch('user/getInfo')
-          if (perrouters && perrouters.length > 0) {
-            // 根据角色，生成可访问权限（路由）
-            const accessRoutes = await store.dispatch('permission/generateRoutes', perrouters)
-            // 动态加载路由
-            router.addRoutes(accessRoutes)
-            // 加载路由完成，跳转
-            // replace: true 不缓存，每次页面都刷新
-            next({...to, replace: true})
-          } else {
+        // 登录后，跳转到原来打开的页面
+        // 跳转前，先判断：store里是否有角色信息
+        const hasRouters = store.getters.perrouters && store.getters.perrouters.length > 0
+        if (hasRouters) {
+          next()
+        } else {
+          try {
+            // 查询角色信息
+            const {perrouters} = await store.dispatch('user/getInfo')
+            if (perrouters && perrouters.length > 0) {
+              // 根据角色，生成可访问权限（路由）
+              const accessRoutes = await store.dispatch('permission/generateRoutes', perrouters)
+              // 动态加载路由
+              router.addRoutes(accessRoutes)
+              // 加载路由完成，跳转
+              // replace: true 不缓存，每次页面都刷新
+              next({...to, replace: true})
+            } else {
+              await store.dispatch('user/resetToken')
+              next(`/login?redirect=${to.path}`)
+              Message.error('抱歉，您没有权限！')
+              NProgress.done()
+            }
+          } catch (error) {
+            // 出现错误，删除token，重新登录
             await store.dispatch('user/resetToken')
+            // Message.error(error || 'Has Error')
+            Message.error({message: error || 'Has Error'})
             next(`/login?redirect=${to.path}`)
-            Message.error('抱歉，您没有权限！')
             NProgress.done()
           }
-        } catch (error) {
-          // 出现错误，删除token，重新登录
-          await store.dispatch('user/resetToken')
-          // Message.error(error || 'Has Error')
-          Message.error({message: error || 'Has Error'})
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
         }
       }
-    }
-  } else {
-    // 无token
-    if (whiteList.indexOf(to.path) !== -1) {
-      // 查询路由是否在白名单里，是直接跳转
-      next()
     } else {
-      // 否，无权限，跳到登录页
+      // 无token
+      // 无权限，跳到登录页
       next(`/login?redirect=${to.path}`)
       NProgress.done()
     }
