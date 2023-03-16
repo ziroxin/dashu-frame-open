6ffe44bb0eef3e157238bf6aecc14cb0c0d2f8d8
zhaoxin <877185959@qq.com>
2023-03-16 16:28:56 +0800

    Fix: 举例：添加/修改页面，不是列表页弹窗，而是另外一个页面时（子路由全是隐藏路由，不再菜单显示，但需要跳转到）

--- web-vue2/src/store/modules/permission.js
+++ web-vue2/src/store/modules/permission.js
@@ -1,5 +1,5 @@
 import Layout from '@/layout'
-import {errorRoute} from "@/router";
+import {errorRoute} from '@/router';
 
 // 转换成组件
 function convertComponent(component) {
@@ -30,35 +30,41 @@ export function filterAsyncRoutes(routers, isTop) {
   // 遍历路由
   routers.forEach(route => {
       // 是否禁用
-      if (route.permissionIsEnabled) {
+      if (route.permissionIsEnabled && route.permissionIsShow) {
+        // 遍历子路由
+        let thisChild;
         if (route.children) {
-          // 包含子路由的父级路由
-          const temp = {
-            hidden: !route.permissionIsShow,
-            path: route.permissionRouter,
-            component: isTop ? Layout : convertComponent(route.permissionComponent),
-            name: route.permissionName || '',
-            redirect: route.noRedirect,
-            meta: {
-              title: route.permissionTitle,
-              icon: route.permissionIcon || '',
-              activeMenu: !route.permissionIsShow ? route.activeMenu : '',
-              noCache: route.noCache,
-              breadcrumb: route.breadcrumb,
-              affix: route.affix
-            },
-            alwaysShow: true,
-            // 迭代子路由
-            children: filterAsyncRoutes(route.children, false)
+          thisChild = filterAsyncRoutes(route.children, false)
+        }
+        // 普通路由属性
+        let temp = {
+          hidden: !route.permissionIsShow,
+          path: route.permissionRouter,
+          component: isTop ? Layout : convertComponent(route.permissionComponent),
+          name: route.permissionName || '',
+          meta: {
+            title: route.permissionTitle,
+            icon: route.permissionIcon || '',
+            activeMenu: !route.permissionIsShow ? route.activeMenu : '',
+            noCache: route.noCache,
+            breadcrumb: route.breadcrumb,
+            affix: route.affix
           }
-          res.push(temp)
+        }
+        // 是否有子路由
+        if (thisChild && thisChild.length > 0) {
+          // 有子路由（配置子路由属性）
+          temp = {
+            ...temp,
+            redirect: route.noRedirect,
+            alwaysShow: true,
+            children: thisChild
+          }
         } else {
           if (isTop) {
-            // 顶级路由
-            const temp = {
-              hidden: !route.permissionIsShow,
-              path: route.permissionRouter,
-              component: Layout,
+            // 无子路由，但是顶级路由（特殊处理）
+            temp = {
+              ...temp,
               redirect: route.noRedirect,
               children: [{
                 path: 'index',
@@ -74,25 +80,52 @@ export function filterAsyncRoutes(routers, isTop) {
                 }
               }]
             }
-            res.push(temp)
-          } else {
-            // 子路由（不能配置redirect）
-            const temp = {
-              hidden: !route.permissionIsShow,
-              path: route.permissionRouter,
-              component: convertComponent(route.permissionComponent),
-              name: route.permissionName || '',
-              meta: {
-                title: route.permissionTitle,
-                icon: route.permissionIcon || '',
-                activeMenu: !route.permissionIsShow ? route.activeMenu : '',
-                noCache: route.noCache,
-                breadcrumb: route.breadcrumb,
-                affix: route.affix
-              }
-            }
-            res.push(temp)
           }
+        }
+        res.push(temp)
+      }
+    }
+  )
+  return res
+}
+
+/**
+ * 迭代（递归）循环出动态路由 - 专门处理隐藏路由
+ */
+export function filterAsyncRoutes4Hidden(routers, isTop) {
+  const res = []
+  // 遍历路由
+  routers.forEach(route => {
+      // 是否禁用
+      if (route.permissionIsEnabled) {
+        if (!route.permissionIsShow) {
+          // 普通路由属性
+          let temp = {
+            hidden: !route.permissionIsShow,
+            path: route.permissionRouter,
+            // component: isTop ? Layout : convertComponent(route.permissionComponent),
+            component: Layout,
+            children: [
+              {
+                path: route.permissionRouter,
+                component: convertComponent(route.permissionComponent),
+                name: route.permissionName || '',
+                meta: {
+                  title: route.permissionTitle,
+                  icon: route.permissionIcon || '',
+                  activeMenu: !route.permissionIsShow ? route.activeMenu : '',
+                  noCache: route.noCache,
+                  breadcrumb: route.breadcrumb,
+                  affix: route.affix
+                }
+              }
+            ]
+          }
+          res.push(temp)
+        }
+        // 遍历子路由
+        if (route.children) {
+          res.push(...filterAsyncRoutes4Hidden(route.children, false))
         }
       }
     }
@@ -113,10 +146,14 @@ const mutations = {
 const actions = {
   generateRoutes({commit}, routers) {
     return new Promise(resolve => {
-      // 动态生成菜单
-      const accessedRoutes = filterAsyncRoutes(routers, true)
+      // 动态生成菜单（排除隐藏路由）
+      let accessedRoutes = filterAsyncRoutes(routers, true)
+      // 单独处理隐藏路由
+      accessedRoutes.push(...filterAsyncRoutes4Hidden(routers, true))
+      // 404路由
+      accessedRoutes.concat(errorRoute)
       commit('SET_ROUTES', accessedRoutes)
-      resolve(accessedRoutes.concat(errorRoute))
+      resolve(accessedRoutes)
     })
   }
 }
