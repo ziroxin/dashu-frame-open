f88617d3b333d45af7424818b36cd548b80a4d61
zhaoxin <877185959@qq.com>
2023-03-28 16:25:33 +0800

    Add: 重写Security用的加密方法（当无法获取到明文密码时，可以使用密文验证密码是否正确）

--- core/src/main/java/com/kg/core/security/config/WebSecurityConfig.java
+++ core/src/main/java/com/kg/core/security/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.CharsetUtil;
 import com.kg.core.filter.JwtTokenAuthenticationFilter;
 import com.kg.core.security.handler.SimpleAccessDeniedHandler;
 import com.kg.core.security.handler.SimpleAuthenticationEntryPoint;
+import com.kg.core.security.util.MyPasswordEncoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.util.StringUtils;
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new MyPasswordEncoder();
     }
 
     /**
