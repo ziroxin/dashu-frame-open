25255e02a7f8c6697d0518f4d6c8dd1be018141c
zhaoxin <877185959@qq.com>
2023-03-29 08:59:13 +0800

    Update: Excel工具，增加读取excel（为导入做准备）

--- component/src/main/java/com/kg/component/office/ExcelCommonUtils.java
+++ component/src/main/java/com/kg/component/office/ExcelCommonUtils.java
@@ -1,13 +1,20 @@
 package com.kg.component.office;
 
+import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.ExcelWriter;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Excel常用操作 - 工具类
@@ -21,9 +28,9 @@ public class ExcelCommonUtils {
      * 导出excel文件（简单表头）
      *
      * @param path      文件生成路径
-     * @param title     第一行标题
+     * @param title     第一行标题（大标题）
      * @param dataList  数据集合（实体类）
-     * @param columnMap 字段对应关系，例如：{"name":"姓名","age":"年龄"}
+     * @param columnMap 字段-标题 对应关系（按顺序导出），例如：{"name":"姓名","age":"年龄","字段名":"标题字段名"}
      * @return 是否导出成功
      */
     public static boolean write(String path, String title, List<?> dataList, LinkedHashMap<String, String> columnMap) {
@@ -72,4 +79,33 @@ public class ExcelCommonUtils {
         }
     }
 
+    /**
+     * 读取Excel - 导入Excel
+     *
+     * @param request        请求信息
+     * @param headerRowIndex 标题所在行（从0开始计数）
+     * @param startRowIndex  数据起始行（包含，从0开始计数）
+     * @param clazz          返回值数据集合（实体类）
+     * @param alias          字段-标题 对应关系，例如：{"name":"姓名","age":"年龄","字段名":"标题字段名"}
+     * @return 读取出的实体列表
+     */
+    public static <T> List<T> read(HttpServletRequest request, int headerRowIndex, int startRowIndex,
+                                   Class clazz, Map<String, String> alias) {
+        try {
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            MultipartFile multipartFile = multipartRequest.getFile(multipartRequest.getFileMap().entrySet().stream().findFirst().get().getKey());
+            InputStream inputStream = multipartFile.getInputStream();
+            //读取文件为ExcelReader对象
+            ExcelReader reader = ExcelUtil.getReader(inputStream);
+            //设置标题别名
+            if (alias != null) {
+                reader.setHeaderAlias(alias);
+            }
+            List<T> dataList = reader.read(headerRowIndex, startRowIndex, clazz);
+            //返回集合
+            return dataList;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
