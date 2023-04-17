9f8e09c3d6a479ee608f018af37295337cbfaa2c
zhaoxin <877185959@qq.com>
2023-04-17 15:09:04 +0800

    Update: 解决多人开发后台，权限缓存问题（例如：在A处保存权限关联关系，则在A处更新缓存，B处未更新的问题）

--- module/src/main/java/com/kg/DashuApplication.java
+++ module/src/main/java/com/kg/DashuApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 // 开启声明式事务
 @EnableTransactionManagement
---
+++ module/src/main/java/com/kg/DashuApplicationRunner.java
@@ -0,0 +1,32 @@
+package com.kg;
+
+import com.kg.component.redis.RedisUtils;
+import com.kg.core.common.constant.LoginConstant;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * 项目启动后，执行一些初始化操作
+ *
+ * @author ziro
+ * @date 2023-04-17 14:58:44
+ */
+@Component
+public class DashuApplicationRunner implements ApplicationRunner {
+    @Resource
+    private RedisUtils redisUtils;
+
+    /**
+     * 项目启动后：进行初始化操作
+     */
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 清空用户授权信息缓存
+        redisUtils.delete(LoginConstant.ROLE_API_REDIS_KEY);
+        // 清空安全配置缓存
+        redisUtils.delete(LoginConstant.SAFETY_SET_REDIS_KEY);
+    }
+}
