8225cf54fb3b3f724bc671129f22ed9e87b4fc10
wangning666 <1192576092@qq.com>
2023-05-16 10:34:23 +0800

    feat:添加是否需要防止重复提交逻辑，新增本地存储以及会话存储方法

--- web-vue2/src/utils/request.js
+++ web-vue2/src/utils/request.js
@@ -4,6 +4,7 @@ import store from '@/store'
 import {getToken, getTokenValidTime} from '@/utils/auth'
 import qs from 'qs'
 import {isWhiteList} from '@/router/white-list'
+import cache from '@/utils/cache'
 
 // 创建axios
 const service = axios.create({
@@ -39,6 +40,31 @@ service.interceptors.request.use(
       // 若是是get请求，且params是数组类型如arr=[1,2]，则转换成arr=1&arr=2
       config.paramsSerializer = function (params) {
         return qs.stringify(params, {arrayFormat: 'repeat'})
+      }
+    }
+     // 是否需要防止数据重复提交
+    const isRepeatSubmit = (config.headers || {}).repeatSubmit === false
+    if (!isRepeatSubmit && (config.method === 'post' || config.method === 'put')) {
+      const requestObj = {
+        url: config.url,
+        data: typeof config.data === 'object' ? JSON.stringify(config.data) : config.data,
+        time: new Date().getTime()
+      }
+      const sessionObj = cache.session.getJSON('sessionObj')
+      if (sessionObj === undefined || sessionObj === null || sessionObj === '') {
+        cache.session.setJSON('sessionObj', requestObj)
+      } else {
+        const s_url = sessionObj.url // 请求地址
+        const s_data = sessionObj.data // 请求数据
+        const s_time = sessionObj.time // 请求时间
+        const interval = 1000 // 间隔时间(ms)，小于此时间视为重复提交
+        if (s_data === requestObj.data && requestObj.time - s_time < interval && s_url === requestObj.url) {
+          const message = '数据正在处理，请勿重复提交'
+          console.warn(`[${s_url}]: ` + message)
+          return Promise.reject(new Error(message))
+        } else {
+          cache.session.setJSON('sessionObj', requestObj)
+        }
       }
     }
     return config
