5d4e577e4080a11e1a2311dbab3139976e1559dc
ziro <877185959@qq.com>
2023-08-03 15:33:15 +0800

    Fix: 修复permission.js登录后/login路由判断bug

--- web-vue2/src/permission.js
+++ web-vue2/src/permission.js
@@ -19,6 +19,14 @@ router.beforeEach(async (to, from, next) => {
   // 设置标题
   document.title = getPageTitle(to.meta.title)
 
+  // 单独判断login
+  if (to.path === '/login') {
+    if (getToken()) {
+      // 登录后，跳转到首页
+      next({path: '/'})
+      NProgress.done()
+    }
+  }
   // 路由白名单，直接跳转（不需检测token）
   if (isWhiteList(to.path)) {
     next()
@@ -28,42 +36,36 @@ router.beforeEach(async (to, from, next) => {
     const hasToken = getToken()
 
     if (hasToken) {
-      if (to.path === '/login') {
-        // 登录后，跳转到首页
-        next({path: '/'})
-        NProgress.done()
+      // 登录后，跳转到原来打开的页面
+      // 跳转前，先判断：store里是否有角色信息
+      const hasRouters = store.getters.perrouters && store.getters.perrouters.length > 0
+      if (hasRouters) {
+        next()
       } else {
-        // 登录后，跳转到原来打开的页面
-        // 跳转前，先判断：store里是否有角色信息
-        const hasRouters = store.getters.perrouters && store.getters.perrouters.length > 0
-        if (hasRouters) {
-          next()
-        } else {
-          try {
-            // 查询角色信息
-            const {perrouters} = await store.dispatch('user/getInfo')
-            if (perrouters && perrouters.length > 0) {
-              // 根据角色，生成可访问权限（路由）
-              const accessRoutes = await store.dispatch('permission/generateRoutes', perrouters)
-              // 动态加载路由
-              router.addRoutes(accessRoutes)
-              // 加载路由完成，跳转
-              // replace: true 不缓存，每次页面都刷新
-              next({...to, replace: true})
-            } else {
-              await store.dispatch('user/resetToken')
-              next(`/login?redirect=${to.path}`)
-              Message.error('抱歉，您没有权限！')
-              NProgress.done()
-            }
-          } catch (error) {
-            // 出现错误，删除token，重新登录
+        try {
+          // 查询角色信息
+          const {perrouters} = await store.dispatch('user/getInfo')
+          if (perrouters && perrouters.length > 0) {
+            // 根据角色，生成可访问权限（路由）
+            const accessRoutes = await store.dispatch('permission/generateRoutes', perrouters)
+            // 动态加载路由
+            router.addRoutes(accessRoutes)
+            // 加载路由完成，跳转
+            // replace: true 不缓存，每次页面都刷新
+            next({...to, replace: true})
+          } else {
             await store.dispatch('user/resetToken')
-            // Message.error(error || 'Has Error')
-            Message.error({message: error || 'Has Error'})
             next(`/login?redirect=${to.path}`)
+            Message.error('抱歉，您没有权限！')
             NProgress.done()
           }
+        } catch (error) {
+          // 出现错误，删除token，重新登录
+          await store.dispatch('user/resetToken')
+          // Message.error(error || 'Has Error')
+          Message.error({message: error || 'Has Error'})
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
         }
       }
     } else {
