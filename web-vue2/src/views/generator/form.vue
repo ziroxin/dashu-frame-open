cf50da0e5e1f5f55d322adc8b8e7929e091dbed3
ziroxin <877185959@qq.com>
2024-01-11 14:09:17 +0800

    Update: 优化代码生成器，页面加载时收起左侧菜单，页面销毁时打开左侧菜单

--- web-vue2/src/views/generator/form.vue
+++ web-vue2/src/views/generator/form.vue
@@ -5,18 +5,33 @@
   </div>
 </template>
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
   data() {
     return {
-      htmlPageURL: 'form-generator/index.html'
+      htmlPageURL: 'form-generator/index.html',
     }
   },
+  computed: {
+    ...mapGetters(['sidebar'])
+  },
   created() {
+    if (this.sidebar.opened) {
+      // 关闭左侧菜单
+      this.$store.dispatch('app/toggleSideBar')
+    }
     if (this.$route.query.openType === 'new') {
       this.htmlPageURL = 'form-generator/index.html#/?openType=new'
     }
     if (this.$route.query.fid) {
       this.htmlPageURL = 'form-generator/index.html#/?fid=' + this.$route.query.fid
+    }
+  },
+  beforeDestroy() {
+    if (!this.sidebar.opened) {
+      // 恢复左侧菜单
+      this.$store.dispatch('app/toggleSideBar')
     }
   }
 }
