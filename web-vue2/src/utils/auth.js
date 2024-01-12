a805792fc89c4a56df41a15b87ec8e3f27f10ae5
ziroxin <877185959@qq.com>
2024-01-12 14:37:33 +0800

    Update: 优化前端token刷新时间配置

--- web-vue2/src/utils/auth.js
+++ web-vue2/src/utils/auth.js
@@ -4,16 +4,19 @@ import Cookies from 'js-cookie'
 const TokenKey = 'Admin-Token'
 // token过期时间
 const TokenValidTimeKey = 'Admin-Token-Valid-Time'
+// token到期前多久，刷新token（默认：10分钟；每次请求api前检测request.js中）
+// 注意：该时间必须大于，后台配置的jwtToken有效期！！！
+const TokenRefreshInterval = 10 * 60 * 1000
 // 用户关闭浏览器保持登录状态：默认关闭 false;开启 true
 // （若开启，则用户登录状态在：token到期后，或者用户手动清理cookie，才失效）
-const IsTokenRemeber = false
+const IsTokenRemember = false
 
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token, validTime) {
-  if (IsTokenRemeber) {
+  if (IsTokenRemember) {
     if (validTime) {
       return Cookies.set(TokenKey, token, {expires: validTime})
     }
@@ -34,8 +37,12 @@ export function getTokenValidTime() {
 }
 
 export function setTokenValidTime(validTime) {
-  if (IsTokenRemeber) {
+  if (IsTokenRemember) {
     return Cookies.set(TokenValidTimeKey, validTime, {expires: validTime})
   }
   return Cookies.set(TokenValidTimeKey, validTime)
+}
+
+export function getTokenRefreshInterval() {
+  return TokenRefreshInterval
 }
