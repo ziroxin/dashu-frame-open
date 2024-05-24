d9997f2465c16be66508d349317414a146c3f7d3
ziroxin <877185959@qq.com>
2024-05-24 16:18:39 +0800

    Update：更新jar版本；优化部分代码细节

--- component/src/main/java/com/kg/component/utils/QuartzManagerUtils.java
+++ component/src/main/java/com/kg/component/utils/QuartzManagerUtils.java
@@ -14,10 +14,7 @@ import java.util.Map;
  */
 @Component
 public class QuartzManagerUtils {
-
-    /**
-     * 注入任务调度器
-     */
+    // 注入任务调度器
     @Resource
     private Scheduler scheduler;
     // 任务组
