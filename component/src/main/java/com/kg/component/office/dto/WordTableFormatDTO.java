7408af8a2265d25f2c05c0a2030396f17ef83a03
ziroxin <877185959@qq.com>
2024-05-24 16:17:32 +0800

    Update：更新word导出工具，表格边框样式配置

--- component/src/main/java/com/kg/component/office/dto/WordTableFormatDTO.java
+++ component/src/main/java/com/kg/component/office/dto/WordTableFormatDTO.java
@@ -2,10 +2,7 @@ package com.kg.component.office.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
-import org.apache.poi.xwpf.usermodel.TableRowAlign;
-import org.apache.poi.xwpf.usermodel.TableWidthType;
-import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.*;
 
 /**
  * Word 表格 - 常用属性
@@ -26,6 +23,14 @@ public class WordTableFormatDTO {
     private String width;
     // 表格对齐方式（该表格在行内的对齐方式，不是表格内容的对齐方式）
     private TableRowAlign align;
+    // 表格边框属类型（无边框设置为：XWPFTable.XWPFBorderType.NONE）
+    private XWPFTable.XWPFBorderType borderType;
+    // 表格边框宽度（无边框时无效）
+    private int borderSize;
+    // 表格边框空隙（无边框时无效）
+    private int borderSpace;
+    // 表格边框颜色（无边框时无效）
+    private String borderColor;
     // 行高
     private int rowHeight;
     // 单元格对齐方式
@@ -46,11 +51,25 @@ public class WordTableFormatDTO {
         widthType = TableWidthType.PCT;// 宽度类型：百分比
         width = "100%";// 宽度：100%
         align = TableRowAlign.CENTER;// 对齐方式：居中
+        borderType = XWPFTable.XWPFBorderType.SINGLE;// 边框类型：单线
+        borderSize = 1;// 边框宽度：1
+        borderSpace = 0;// 边框空隙：0
+        borderColor = "000000";// 边框颜色：黑色
         rowHeight = 400;// 行高：400
         cellAlign = ParagraphAlignment.CENTER;// 单元格对齐方式：居中
         cellValign = XWPFTableCell.XWPFVertAlign.CENTER;// 单元格垂直对齐方式：居中
         cellSpaceBefore = 0;// 单元格段前空白：0
         cellSpaceAfter = 0;// 单元格段后空白：0
         bgColor = "FFFFFF";// 单元格背景色：白色
+    }
+
+    /**
+     * 设置无边框
+     */
+    public void setNoneBorder() {
+        borderType = XWPFTable.XWPFBorderType.NONE;
+        borderSize = 0;
+        borderSpace = 0;
+        borderColor = null;
     }
 }
