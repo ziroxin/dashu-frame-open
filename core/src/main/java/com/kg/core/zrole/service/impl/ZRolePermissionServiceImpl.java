fbeb6122b519bcb1c8877e0924a1b460e674ec75
ziroxin <877185959@qq.com>
2024-08-07 17:33:58 +0800

    Fix: 修复 保存角色和权限关联关系，未清理缓存bug

--- core/src/main/java/com/kg/core/zrole/service/impl/ZRolePermissionServiceImpl.java
+++ core/src/main/java/com/kg/core/zrole/service/impl/ZRolePermissionServiceImpl.java
@@ -1,14 +1,17 @@
 package com.kg.core.zrole.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kg.component.redis.RedisUtils;
+import com.kg.core.common.constant.CacheConstant;
 import com.kg.core.zrole.dto.ZRolePermissionSaveDTO;
 import com.kg.core.zrole.entity.ZRolePermission;
 import com.kg.core.zrole.mapper.ZRolePermissionMapper;
 import com.kg.core.zrole.service.IZRolePermissionService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,10 +26,15 @@ import java.util.stream.Collectors;
  */
 @Service
 public class ZRolePermissionServiceImpl extends ServiceImpl<ZRolePermissionMapper, ZRolePermission> implements IZRolePermissionService {
+    @Resource
+    private RedisUtils redisUtils;
 
     @Override
     @Transactional(rollbackFor = RuntimeException.class)
     public void saveRolePermission(ZRolePermissionSaveDTO rolePermissionSaveDTO) {
+        // 清除角色权限关联关系缓存
+        redisUtils.delete(CacheConstant.ROLE_API_REDIS_KEY);
+
         List<ZRolePermission> collect = Arrays.stream(rolePermissionSaveDTO.getPermissionIds())
                 .map(permissionId -> {
                     ZRolePermission rolePermission = new ZRolePermission();
