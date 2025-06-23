09bd84e6f66f97740ba9eecf8546d2fd81f30d2d
ziroxin <877185959@qq.com>
2025-06-23 14:12:01 +0800

    update: 优化代码格式

--- web-vue3/src/api/login/index.ts
+++ web-vue3/src/api/login/index.ts
@@ -6,29 +6,21 @@ interface RoleParams {
 }
 
 export const loginApi = (data: UserType): Promise<IResponse<UserType>> => {
-  return request.post({ url: '/mock/user/login', data })
+  return request.post({url: '/mock/user/login', data})
 }
 
 export const loginOutApi = (): Promise<IResponse> => {
-  return request.get({ url: '/mock/user/loginOut' })
+  return request.get({url: '/mock/user/loginOut'})
 }
 
-export const getUserListApi = ({ params }: AxiosConfig) => {
-  return request.get<{
-    code: string
-    data: {
-      list: UserType[]
-      total: number
-    }
-  }>({ url: '/mock/user/list', params })
+export const getUserListApi = ({params}: AxiosConfig) => {
+  return request.get<{ code: string, data: { list: UserType[], total: number } }>({url: '/mock/user/list', params})
 }
 
-export const getAdminRoleApi = (
-  params: RoleParams
-): Promise<IResponse<AppCustomRouteRecordRaw[]>> => {
-  return request.get({ url: '/mock/role/list', params })
+export const getAdminRoleApi = (params: RoleParams): Promise<IResponse<AppCustomRouteRecordRaw[]>> => {
+  return request.get({url: '/mock/role/list', params})
 }
 
 export const getTestRoleApi = (params: RoleParams): Promise<IResponse<string[]>> => {
-  return request.get({ url: '/mock/role/list2', params })
+  return request.get({url: '/mock/role/list2', params})
 }
