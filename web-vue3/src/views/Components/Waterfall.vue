9183178b169226fe90dd76cc482bacd324d13fe3
ziroxin <877185959@qq.com>
2025-06-23 13:51:12 +0800

    delete: 移除plop和mockjs相关代码，补充

--- web-vue3/src/views/Components/Waterfall.vue
+++ web-vue3/src/views/Components/Waterfall.vue
@@ -2,7 +2,6 @@
 import { Waterfall } from '@/components/Waterfall'
 import { ContentWrap } from '@/components/ContentWrap'
 import { useI18n } from '@/hooks/web/useI18n'
-import Mock from 'mockjs'
 import { ref, unref } from 'vue'
 import { toAnyString } from '@/utils'
 
@@ -12,17 +11,9 @@ const getList = () => {
   const list: any = []
   for (let i = 0; i < 20; i++) {
     // 随机 100, 500 之间的整数
-    const height = Mock.Random.integer(100, 500)
-    const width = Mock.Random.integer(100, 500)
-    list.push(
-      Mock.mock({
-        width,
-        height,
-        id: toAnyString(),
-        // http更换为https
-        image_uri: Mock.Random.image(`${width}x${height}`).replace('http://', 'https://')
-      })
-    )
+    const width = Math.floor(Math.random() * (500 - 100 + 1)) + 100
+    const height = Math.floor(Math.random() * (500 - 100 + 1)) + 100
+    list.push({width, height, id: toAnyString(), image_uri: `https://dummyimage.com/${width}x${height}`})
   }
   data.value = [...unref(data), ...list]
   if (unref(data).length >= 60) {
@@ -31,7 +22,7 @@ const getList = () => {
 }
 getList()
 
-const { t } = useI18n()
+const {t} = useI18n()
 
 const loading = ref(false)
 
@@ -48,15 +39,7 @@ const loadMore = () => {
 
 <template>
   <ContentWrap :title="t('router.waterfall')">
-    <Waterfall
-      :data="data"
-      :loading="loading"
-      :end="end"
-      :props="{
-        src: 'image_uri',
-        height: 'height'
-      }"
-      @load-more="loadMore"
-    />
+    <Waterfall :data="data" :loading="loading" :end="end"
+               :props="{src: 'image_uri',height: 'height'}" @load-more="loadMore"/>
   </ContentWrap>
 </template>
