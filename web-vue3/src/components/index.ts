f7570fa126087caaacb512b07c4544897e88420b
ziroxin <877185959@qq.com>
2025-06-25 11:13:51 +0800

    docs(core): 添加setupGlobCom函数注释

--- web-vue3/src/components/index.ts
+++ web-vue3/src/components/index.ts
@@ -3,6 +3,10 @@ import { Icon } from './Icon'
 import { Permission } from './Permission'
 import { BaseButton } from './Button'
 
+/**
+ * 常用自定义组件全局引入，无需在每个vue文件中单独引入
+ * @param app Vue实例
+ */
 export const setupGlobCom = (app: App<Element>): void => {
   app.component('Icon', Icon)
   app.component('Permission', Permission)
