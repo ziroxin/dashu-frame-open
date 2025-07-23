8885e55f6c5aadef8704c3b726163af2cad18fa8
ziroxin <877185959@qq.com>
2025-07-23 16:26:34 +0800

    refactor(ui): 更新ElementPlus全局配置并优化样式

--- web-vue3/src/App.vue
+++ web-vue3/src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <ConfigGlobal :size="currentSize">
-    <RouterView :class="greyMode ? `${prefixCls}-grey-mode` : ''"/>
-  </ConfigGlobal>
+  <config-global :size="currentSize">
+    <router-view :class="greyMode ? `${prefixCls}-grey-mode` : ''"/>
+  </config-global>
 </template>
+
 <script setup lang="ts">
 import { useAppStore } from '@/store/modules/app'
 import { ConfigGlobal } from '@/components/ConfigGlobal'
 import { useDesign } from '@/hooks/web/useDesign'
 
-const {getPrefixCls} = useDesign()
-const prefixCls = getPrefixCls('app')
+// 灰度模式判断
+const prefixCls = useDesign().getPrefixCls('app')
 const appStore = useAppStore()
-const currentSize = computed(() => appStore.getCurrentSize)
 const greyMode = computed(() => appStore.getGreyMode)
+// 初始化主题
 appStore.initTheme()
 </script>
+
 <style lang="less">
 @prefix-cls: ~'@{adminNamespace}-app';
-
 .size {
   width: 100%;
   height: 100%;
@@ -29,7 +30,6 @@ body {
   margin: 0;
   overflow: hidden;
   .size;
-
   #app {
     .size;
   }
--- web-vue3/src/components/ConfigGlobal/src/ConfigGlobal.vue
+++ web-vue3/src/components/ConfigGlobal/src/ConfigGlobal.vue
@@ -1,43 +1,48 @@
 <template>
-  <ElConfigProvider :namespace="variables.elNamespace" :message="{max: 1}" :size="size">
+  <!-- ElementPlus的全局配置 @link https://element-plus.org/zh-CN/component/config-provider.html  -->
+  <el-config-provider :namespace="variables.elNamespace" :message="currentMessage" :size="currentSize"
+                      :link="currentLink" :locale="currentLocale">
     <slot></slot>
-  </ElConfigProvider>
+  </el-config-provider>
 </template>
 <script setup lang="ts">
-import { onMounted, provide, watch } from 'vue'
-import { propTypes } from '@/utils/propTypes'
-import { ComponentSize, ElConfigProvider } from 'element-plus'
 import { useWindowSize } from '@vueuse/core'
 import { useAppStore } from '@/store/modules/app'
 import { setCssVar } from '@/utils'
 import { useDesign } from '@/hooks/web/useDesign'
+import { useLocaleStoreWithOut } from '@/store/modules/locale'
 
 const {variables} = useDesign()
-
 const appStore = useAppStore()
-
-const props = defineProps({
-  size: propTypes.oneOf<ComponentSize>(['default', 'small', 'large']).def('default')
-})
-
-provide('configGlobal', props)
 
 // 初始化所有主题色
 onMounted(() => { appStore.setCssVarTheme() })
 
-const {width} = useWindowSize()
+// 消息：{max:10(可同时显示的消息最大数量，默认不限制), grouping:true(合并内容相同的消息)}
+const currentMessage = computed(() => ({grouping: true}))
 
-// 监听窗口变化
-watch(() => width.value,
-    (width: number) => {
-      if (width < 768) {
-        !appStore.getMobile ? appStore.setMobile(true) : undefined
-        setCssVar('--left-menu-min-width', '0')
-        appStore.setCollapse(true)
-        appStore.getLayout !== 'classic' ? appStore.setLayout('classic') : undefined
-      } else {
-        appStore.getMobile ? appStore.setMobile(false) : undefined
-        setCssVar('--left-menu-min-width', '64px')
-      }
-    }, {immediate: true})
+// 主题大小：default, large, small
+const currentSize = computed(() => appStore.getCurrentSize || 'default')
+
+// 链接样式：{underline: 'hover' (控制下划线是否出现 'always' | 'hover' | 'never' | boolean)}
+const currentLink = ref({underline: 'hover'})
+
+// 设置语言
+const currentLocale = unref(useLocaleStoreWithOut().currentLocale).elLocale
+
+// 监听窗口变化，切换合适的主题模式
+const {width} = useWindowSize()
+watch(() => width.value, (width: number) => {
+  if (width < 768) {
+    // 移动端模式
+    !appStore.getMobile ? appStore.setMobile(true) : undefined
+    setCssVar('--left-menu-min-width', '0')
+    appStore.setCollapse(true)
+    appStore.getLayout !== 'classic' ? appStore.setLayout('classic') : undefined
+  } else {
+    // 其他模式
+    appStore.getMobile ? appStore.setMobile(false) : undefined
+    setCssVar('--left-menu-min-width', '64px')
+  }
+}, {immediate: true})
 </script>
