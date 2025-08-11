c5371df55605d6044616bb87db20211b0500a7a8
ziroxin <877185959@qq.com>
2025-08-11 11:25:16 +0800

    refactor: 修改element-plus改为全量引入（删除按需引入，作用不大），补充1

--- web-vue3/src/views/icons/svg-icons.ts
+++ web-vue3/src/views/icons/svg-icons.ts
@@ -2,7 +2,7 @@
  * 扫描svg目录下所有svg文件
  */
 const svgs = import.meta.glob('../../assets/svgs/*.svg')
-const svgIcons = []
+const svgIcons: any[] = []
 Object.keys(svgs).forEach(name => {
   const svg = name.replace('../../assets/svgs/', '').replace(/(\.\/|\.svg)/g, '')
   svgIcons.push(svg)
